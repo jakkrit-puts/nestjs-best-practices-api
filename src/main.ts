9b08ed42f5e0790225da867f18800ebffedb96c7
jakkrit putsorn <jakkrit.puts@gmail.com>
2022-08-30 17:37:07 +0700

    set up project & connect db

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
-  await app.listen(3000);
+
+  const serverConfig = config.get('server');
+
+  console.log('Starting...#1');
+  console.log(serverConfig.port);
+
+  await app.listen(serverConfig.port);
 }
 bootstrap();
