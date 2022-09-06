479133e99bea27800391ccaaab551f879a1468b4
jakkrit putsorn <jakkrit.puts@gmail.com>
2022-09-06 12:05:47 +0700

    [ADD]: shared  message throw, response

---
+++ src/shared/message/index.ts
@@ -0,0 +1,9 @@
+// success response message
+export const GET_DATA_SUCCESS = 'get data successfully';
+export const CREATE_DATA_SUCCESS = 'create successfully';
+export const UPDATE_DATA_SUCCESS = 'update data successfully';
+export const DELETE_DATA_SUCCESS = 'remove data successfully';
+export const AUTH_SUCCESS = 'signin successfully.';
+
+// throw error response message
+export const NOT_FOUND_DATA = 'Not Found Data.';
